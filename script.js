d99a5ae3dac54670de204436f88adc4e64c4d6b9
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2024-06-08 00:08:35 -0400

    Added the js script to make a slideshow with the images

---
+++ script.js
@@ -0,0 +1,15 @@
+let slideIndex = 0;
+changeSlides();
+
+function changeSlides() {
+  let slides = document.getElementsByClassName("slides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }    
+  slides[slideIndex].style.display = "block";  
+  setTimeout(changeSlides, 2000); 
+}
