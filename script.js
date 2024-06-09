b9334351df848abbc5f1257c0ed91dbce2c7fd4c
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2024-06-09 18:51:05 -0400

    Menus d'options ont étés ajoutés a rendez-vous on peut selectionner multiples services

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let slideIndex = 0;
 changeSlides();
+generateTimeOptions();
 
 function changeSlides() {
   let slides = document.getElementsByClassName("slides");
@@ -13,3 +14,53 @@ function changeSlides() {
   slides[slideIndex].style.display = "block";  
   setTimeout(changeSlides, 2000); 
 }
+
+  function generateTimeOptions() {
+    let select = document.getElementById("heureDropdown");
+    let hour = 9;
+    
+    while(hour < 13)
+      {
+        let option = document.createElement("option");
+        let formattedHour = (hour < 10 ? "0" : "") + hour + ":00";
+        option.text = formattedHour + " AM";
+        option.value = formattedHour;
+        select.add(option);
+        hour++;
+    }
+
+    hour = 1;
+    while(hour<7){
+      let option = document.createElement("option");
+      let formattedHour = (hour < 10 ? "0" : "") + hour + ":00";
+      option.text = formattedHour + " PM";
+      option.value = formattedHour;
+      select.add(option);
+      hour++;
+    }
+
+   
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const addServiceButton = document.getElementById('addServiceButton');
+    const additionalServicesContainer = document.getElementById('additionalServicesContainer');
+    const serviceFormHtml = `
+      <div class="form">
+        <select class="form-control">
+          <option disabled selected value="">Service</option>
+          <option value="seancePhotoMariage">Séance photo de mariage</option>
+          <option value="portraitFamille">Portrait de famille</option>
+          <option value="seancePhotoIndividuelle">Séance photo individuelle</option>
+          <option value="evenements">Événements</option>
+        </select>
+      </div>
+    `;
+  
+    addServiceButton.addEventListener('click', () => {
+      additionalServicesContainer.insertAdjacentHTML('beforeend', serviceFormHtml);
+    });
+  });
+
+  
+  
